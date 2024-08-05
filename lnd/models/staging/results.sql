e7e0c20a904d47e1575840d0bebd98c154a53c22
Marion Geary Bauman <gearymf@gmail.com>
2024-08-05 17:06:32 -0400

    Make updates to data model, begin dashboard

--- lnd/models/staging/results.sql
+++ lnd/models/staging/results.sql
@@ -8,16 +8,16 @@ SELECT DISTINCT
     team_id,
     "Age" as age, "Height" as height, "Weight" as weight
 FROM {{ ref('athlete_events') }}
-    FULL JOIN {{ ref('athlete') }} as a
+    FULL JOIN {{ ref('athletes') }} as a
     ON "ID" = a.athlete_id
-    FULL JOIN {{ ref('team') }} as t
+    FULL JOIN {{ ref('teams') }} as t
     ON "Team" = t.name
-    FULL JOIN {{ ref('country') }} as c
+    FULL JOIN {{ ref('countries') }} as c
     ON "NOC" = c.code
-    JOIN {{ ref('game') }} as g
+    JOIN {{ ref('games') }} as g
     ON "Year" = g.year AND "Season" = g.season AND "City" = g.city
-    JOIN {{ ref('event') }} as e
+    JOIN {{ ref('events') }} as e
     ON "Event" = e.name
-    JOIN {{ ref('medal') }} as m
+    JOIN {{ ref('medals') }} as m
     ON "Medal" = m.type
 GROUP BY athlete_id, country_id, game_id, event_id, medal_id, team_id, age, height, weight
